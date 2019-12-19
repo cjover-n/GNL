f917f52d9517194462c03e3b830a02426829d84b
Cristina Jover navarro <cjover-n@c3r8s4.42madrid.com>
2019-12-19 18:48:21 +0100

    primera subida, no hace nada, solo es un borrador mierdero

---
+++ get_next_line.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cjover-n <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/12/12 03:58:05 by cjover-n          #+#    #+#             */
+/*   Updated: 2019/12/12 05:30:16 by cjover-n         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+#include <stdlib.h>
+#include <sys/types.h>
+
+int		get_next_line(int fd, char **line);
+
+#endif
