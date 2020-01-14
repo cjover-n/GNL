cdcdbe0a1f3c75016cdcc2bfc174c8566804fe46
Cristina Jover navarro <cjover-n@c2r1s5.42madrid.com>
2020-01-14 13:20:48 +0100

    guardar el main por si acaso

---
+++ main.c
@@ -0,0 +1,15 @@
+int     main(void)
+{
+    char    *line;
+    int     fd;
+    int     ret;
+    fd = open("text.txt", O_RDONLY);
+    line = NULL;
+    ret = 1;
+    while (ret)
+    {
+        ret = get_next_line(fd, &line);
+        printf("%s\treturn value - %d\n", line, ret);
+    }
+    close(fd);
+}
